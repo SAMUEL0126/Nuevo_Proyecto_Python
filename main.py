61510e6f71921720d7daf3ab04c545d5d70c1b4f
Samuel Lopez Muñoz <slopezmu@crystal.com.co>
2024-01-25 07:51:33 -0500

    actualizacion readme.md

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+
+from Tkinter import *
+
 import sqlite3
 
-class 
+class Product:
+    def __init__(self,window):
+        self.wind = window
+        self.wind.title('Products Application')
+        
+if __name__ == '__main__':
+    window = Tk()
+    aplication =  Product(window)
+    window.mainloop()
