a07abe23b752074007e4bc9ad9bce29a06d398b6
Samuel Lopez Muñoz <slopezmu@crystal.com.co>
2024-01-29 16:13:20 -0500

    inicio de nuevo proyecto python

--- Reproductor de musica mp3/bryant.py
+++ Reproductor de musica mp3/bryant.py
@@ -15,7 +15,7 @@ def detener_cancion():
     pygame.mixer.music.stop()
 
 if __name__ == '__main__':
-    cancion = '/Reproductor de musica de bryant myers/Apaga_el_celular.mp3' # Ruta de la canción que quieres reproducir
+    cancion = '/Reproductor de musica mp3/Apaga_el_celular.mp3' # Ruta de la canción que quieres reproducir
     reproducir_cancion(cancion)
 
     while True:
